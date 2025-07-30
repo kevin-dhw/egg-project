ae2220e3df36d2c820d6f6bb5e4f718557d78810
kevin <932731452@qq.com>
2025-07-30 15:24:48 +0800

    11

--- .next/types/app/layout.ts
+++ .next/types/app/layout.ts
@@ -1,4 +1,4 @@
-// File: /Users/macbook/Desktop/practice/egg/app/layout.tsx
+// File: /Users/macbook/Desktop/me/egg-project/app/layout.tsx
 import * as entry from '../../../app/layout.js'
 import type { ResolvingMetadata, ResolvingViewport } from 'next/dist/lib/metadata/types/metadata-interface.js'
 
--- .next/types/app/page.ts
+++ .next/types/app/page.ts
@@ -1,4 +1,4 @@
-// File: /Users/macbook/Desktop/practice/egg/app/page.tsx
+// File: /Users/macbook/Desktop/me/egg-project/app/page.tsx
 import * as entry from '../../../app/page.js'
 import type { ResolvingMetadata, ResolvingViewport } from 'next/dist/lib/metadata/types/metadata-interface.js'
 
